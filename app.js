f2eeb49cdc5184db3fd3de812ccc26e14c4dc2fa
Matthew Whitaker <matthew.w3000@gmail.com>
2016-08-18 17:04:40 -0600

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,30 @@ function signOut() {
 }
 
 function deleteAccount() {
+  console.log("Deleting Account...")
   firebase.auth().currentUser.delete();
   console.log("Account Deleted");
+  console.log("Nulling variables...");
+  displayName = undefined;
+  photoURL = null;
+  uid = undefined;
+  email = undefined;
+  provider = undefined;
+  providerUID = undefined;
+  console.log("Variable null finished.");
+  console.log("UI update started...");
+  onAuthInit();
+  console.log("UI update finished.");
+  alert("Account Deleted");
+}
+
+function accountDelete() {
+  var randomNumber = math.random() * 20000000;
+  var a = prompt("Are you sure you want to delete your account? This is a permanent action and cannot be undone! If you are one hundred percent sure that you want to do this, type in this number: "+randomNumber);
+  if(a==randomNumber) {
+    deleteAccount();
+  } else {
+    return;
+  }
+  }
 }
