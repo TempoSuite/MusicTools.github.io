247bf37b23e09921e92e8dfa698c922ac13a38b4
Matthew Whitaker <matthew.w3000@gmail.com>
2016-08-18 16:34:29 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,15 @@
  */
 function signIn() {
   window.location.assign('/widget');
+  console.log("Sign In Flow Started...");
 };
 
 function signOut() {
   firebase.auth().signOut();
+  console.log("Signed Out");
 }
 
 function deleteAccount() {
   firebase.auth().currentUser.delete();
+  console.log("Account Deleted");
 }
