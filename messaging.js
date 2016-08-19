245ebec44ead0c09ae69a739a3940ec94bdaf902
Matthew Whitaker <matthew.w3000@gmail.com>
2016-08-19 16:29:37 -0600

    Update messaging.js

--- messaging.js
+++ messaging.js
@@ -1,1 +1,21 @@
-
+function sendMessage() {
+        console.log("Sending message...");
+        var messageEmail = document.getElementById("message_email").value;
+        var subject = document.getElementById("subject").value;
+        var messageContent = document.getElementById("message_content").value;
+        alert(messageEmail);
+        alert(subject);
+        alert(messageContent);
+        var punctuationless = messageEmail.replace(/['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}‌​~']/g,"");
+        var messageEmailWithoutPunctuation = punctuationless.replace(/\s{2,}/g,"");
+        var userId = firebase.auth().currentUser.uid;
+        firebase.database().ref('/publicusers/' + messageEmailWithoutPunctuation).once('value').then(function(snapshot) {
+          if(snapshot.val() != null) {
+            var otheruid = snapshot.val().uid;
+          } else {
+            alert("The email address you entered is not registered with MusicTools.");
+            return false;
+          }
+        });
+        return false;
+      }
