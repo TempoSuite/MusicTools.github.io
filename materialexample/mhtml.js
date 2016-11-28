26c248473ff8803010cee9f6706a2c4bb46bd1d6
Matthew Whitaker <matthew.w3000@gmail.com>
2016-11-28 16:46:02 -0700

    Create mhtml.js

---
+++ materialexample/mhtml.js
@@ -0,0 +1,33 @@
+//Copyright 2016 Matthew Whitaker/Sub 6 Resources
+funtion toolbar(title, color) {
+  
+}
+function updateMHTMLObjects() {
+  var toolbars = document.getElementsByTagName("toolbar");
+  var contents = document.getElementsByTagName("content");
+  var copyrights = document.getElementsByTagName("copyright");
+  var materialBackgroundColor = "#eeeeee";
+  var materialAccentColor = "#001E8D";
+  var materialSecondaryColor = "#002E9D";
+  var materialTextColor = "#EEEEEE";
+  //Iterate through all toolbars and convert them to cool-looking toolbars!!!
+  for(i = 0; i < toolbars.length; i++) {
+    var toolbarText = toolbars[i].getAttribute("title");
+    var iconOne = toolbars[i].getAttribute("iconOneURL");
+    var iconTwo = toolbars[i].getAttribute("iconTwoURL");
+    var iconThree = toolbars[i].getAttribute("iconThreeURL");
+    var iconFour = toolbars[i].getAttribute("iconFourURL");
+    var iconFive = toolbars[i].getAttribute("iconFiveURL");
+    var iconSix = toolbars[i].getAttribute("iconSixURL");
+    var iconSeven = toolbars[i].getAttribute("iconSevenURL");
+    var iconEight = toolbars[i].getAttribute("iconEightURL");
+    var iconNine = toolbars[i].getAttribute("iconNineURL");
+    var iconTen = toolbars[i].getAttribute("iconTenURL");
+    
+    toolbars[i].style.width = "100%";
+    toolbars[i].style.backgroundColor = materialAccentColor;
+    toolbars[i].style.color = materialTextColor;
+    
+  }
+}
+updateMHTMLObjects();
